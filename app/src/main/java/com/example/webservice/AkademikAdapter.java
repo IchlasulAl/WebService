a2b853477af4f535ff1b98d2ccf2dce8b1a11f71
IchlasulAl <m.ichal7749@mail.unpas.ac.id>
2020-04-10 13:18:06 +0700

    menambahkan code di file xml dan java(belum semua)

--- app/src/main/java/com/example/webservice/AkademikAdapter.java
+++ app/src/main/java/com/example/webservice/AkademikAdapter.java
@@ -1,14 +1,62 @@
 package com.example.webservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class AkademikAdapter extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AkademikAdapter extends RecyclerView.Adapter<AkademikAdapter.ViewHolder> {
+
+    private ArrayList<HashMap<String,String>> postList;
+    private MainActivity activity;
+
+    public AkademikAdapter(ArrayList<HashMap<String,String>> postList,MainActivity mainActivity){
+        this.postList=postList;
+        this.activity=mainActivity;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.adapter_akademik);
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup,int i){
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.adapter_akademik,viewGroup,false);
+        return new ViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder,final int position){
+        final HashMap<String,String> post = postList.get(position);
+        Glide.with(activity).load(post.get("img_url")).into(viewHolder.imgLogo);
+        viewHolder.textSingkatan.setText(post.get("singkatan"));
+        viewHolder.textNama.setText(post.get("nama"));
+        viewHolder.textUrl.setText(post.get("url"));
+    }
+    @Override
+    public int getItemCount(){
+        return postList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imgLogo;
+        TextView textSingkatan;
+        TextView textNama;
+        TextView textUrl;
+
+        public ViewHolder(View view){
+            super(view);
+            imgLogo = (ImageView) view.findViewById(R.id.imgLogo);
+            textSingkatan = (TextView) view.findViewById(R.id.textSingkatan);
+            textNama = (TextView) view.findViewById(R.id.textNama);
+            textUrl = (TextView) view.findViewById(R.id.textUrl);
+        }
+
     }
 }
--- app/src/main/java/com/example/webservice/MainActivity.java
+++ app/src/main/java/com/example/webservice/MainActivity.java
@@ -1,14 +1,84 @@
 package com.example.webservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView listAkademik;
+    LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listAkademik= (RecyclerView) findViewById (R.id.listAkademik);
+        linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+        listAkademik.setLayoutManager(linearLayoutManager);
+        new GetAkademikAsyncTask().execute();
+    }
+
+    private class GetAkademikAsynTask extends AsyncTask<String,String,String>{
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Loading...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params){
+            String respon="";
+            try {
+                String url = "https://sab.if-unpas.org/pertemuan_07/akademik.php?action=get_akademik";
+                respon = CustomeHttp.executeHttpGet(url);
+            }catch (Exception e){
+                respon = e.toString();
+            }
+            return respon;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            try {
+                Log.e("masuk","Respon result->"+result);
+                JSONObject object = new JSONObject(result);
+                ArrayList<HashMap<String,String>> arr = new ArrayList<>();
+                if (object.getString("success").equalsIgnoreCase("1")){
+                    JSONArray array = object.getJSONArray("data");
+                    HashMap<String,String> map;
+                    for (int i=0;i<array.length();i++){
+                        JSONObject jsonObject = array.getJSONObject(i);
+                        map = new HashMap<String,String>();
+                        map.put("img_url",jsonObject.getString("img_url"));
+                        map.put("singkatan",jsonObject.getString("singkatan"));
+                        map.put("nama",jsonObject.getString("nama"));
+                        map.put("url",jsonObject.getString("url"));
+                        arr.add(map);
+                    }
+                }
+                listAkademik.setAdapter(new AkademikAdapter(arr,MainActivity.this));
+            }catch (Exception e){
+                Log.e("Masuk","->"+e.getMessage());
+            }
+        }
     }
 }
