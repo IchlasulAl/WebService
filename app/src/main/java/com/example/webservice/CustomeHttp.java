af702a784fefd5d0d1fd1450fc0ba2a3908855d0
IchlasulAl <m.ichal7749@mail.unpas.ac.id>
2020-04-10 15:39:57 +0700

    menyelesaikan code untuk file java dan xml

--- app/src/main/java/com/example/webservice/CustomeHttp.java
+++ app/src/main/java/com/example/webservice/CustomeHttp.java
@@ -5,6 +5,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.params.ConnManagerParams;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -14,13 +15,14 @@ import org.apache.http.params.HttpParams;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.ArrayList;
 
 public class CustomeHttp {
     public static final int HTTP_TIMEOUT = 30 * 1000;
 
     private static HttpClient mHttpClient;
-    private static HttpClient getHttpClient;{
+    private static HttpClient getHttpClient(){
         if (mHttpClient==null){
             mHttpClient = new DefaultHttpClient();
             final HttpParams params = mHttpClient.getParams();
@@ -58,9 +60,61 @@ public class CustomeHttp {
             }
         }
     }
-    public static  String  executeHttpPostRaw(String url, ArrayList<NamaValuePair> postParameter) throws Exception{
+    public static  String  executeHttpPostRaw(String url, ArrayList<NameValuePair> postParameter) throws Exception{
         BufferedReader in = null;
-        
+        try {
+            HttpClient client = getHttpClient();
+            HttpPost request = new HttpPost(url);
+            UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(postParameter);
+            request.setEntity(formEntity);
+            HttpResponse response = client.execute(request);
+            in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer sb = new StringBuffer("");
+            String line = "";
+            String NL = System.getProperty("line.separator");
+            while ((line = in.readLine())!=null){
+                sb.append(line+NL);
+            }
+            in.close();
+            String result = sb.toString();
+            return result;
+        }
+        finally {
+            if (in != null){
+                try {
+                    in.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
+    public static String executeHttpGet(String url) throws Exception{
+        BufferedReader in = null;
+        try {
+            HttpClient client = getHttpClient();
+            HttpGet request = new HttpGet();
+            request.setURI(new URI(url));
+            HttpResponse response = client.execute(request);
+            in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer sb = new StringBuffer("");
+            String line = "";
+            String NL = System.getProperty("line.separator");
+            while ((line = in.readLine())!=null){
+                sb.append(line+NL);
+            }
+            in.close();
+            String result = sb.toString();
+            return result;
+        } finally {
+            if (in != null){
+                try {
+                    in.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
